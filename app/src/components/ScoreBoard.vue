cfc6a5c985ab091fa54f7aa33a5020027f5bc43a
NotLeo27 <158211664+NotLeo27@users.noreply.github.com>
2025-01-30 12:32:10 -0500

    is this the dagger

--- app/src/components/ScoreBoard.vue
+++ app/src/components/ScoreBoard.vue
@@ -1,3 +1,12 @@
 <template>
   <div></div>
 </template>
+
+<script setup>
+import { reactive, ref } from "vue";
+
+let player1score = 0;
+let player2score = 0;
+</script>
+
+<style scoped></style>
---
+++ app/src/components/SingleDouble.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <form name="">
+      <label for="singles"><input type="radio" id="singles" />Singles</label>
+      <label for="doubles"><input type="radio" id="doubles" />Doubles</label>
+    </form>
+  </div>
+</template>
+
+<script></script>
+
+<style scoped></style>
